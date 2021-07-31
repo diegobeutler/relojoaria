5edb001efedcc234b17d8793eef4c1df07fed943
diego <diegobutler2411@gmail.com>
2021-07-31 14:01:25 -0300

    footer

--- src/main/java/br/edu/utfpr/trabalhoFinalWeb/controller/IndexController.java
+++ src/main/java/br/edu/utfpr/trabalhoFinalWeb/controller/IndexController.java
@@ -1,5 +1,6 @@
 package br.edu.utfpr.trabalhoFinalWeb.controller;
 
+import br.edu.utfpr.trabalhoFinalWeb.repository.ItemRepository;
 import br.edu.utfpr.trabalhoFinalWeb.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,12 +12,18 @@ public class IndexController {
 
 	@Autowired
 	private ItemService itemService;
+	@Autowired
+	private ItemRepository itemRepository;
 	@GetMapping("")
 	public String index(Model model) {
 		model.addAttribute("itens", itemService.findAll());
 		return "index";
-
 	}
+//	@GetMapping("/relogios")
+//	public String index(Model model) {
+////		model.addAttribute("itens", itemRepository.findBy());
+//		return "index";
+//	}
 
 	@GetMapping(value = {"teste", "teste2"})
 	public String teste() {
